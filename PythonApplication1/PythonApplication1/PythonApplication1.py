96134d2c0289660059ce1c9418e850fe44518ba6
sumantro93 <saggy.zone@gmail.com>
2016-04-25 15:50:12 +0530

    quiz app

---
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -0,0 +1,32 @@
+print ( "quiz test")
+
+answer ="0"
+while answer != '4':
+    answer = input( "what is 2+2= ")
+print ( "you are right")
+
+answer1 ="0"
+while answer1 != '4':
+    answer1 = input( "what is 2x2= ")
+print ( "you are right")
+
+answer2 ="0"
+while answer2 != '2':
+    answer2 = input( "what is 10/5 = ")
+print ( "you are right")
+
+answer3 ="0"
+while answer3 != '36':
+    answer3 = input( "what is 12x3= ")
+print ( "you are right")
+
+answer4 ="0"
+while answer4 != '1947':
+    answer4 = input( "what year did india get it's indepence?  ")
+print ( "you are right")
+
+answer5 ="0"
+while answer5 != 'Jaipur':
+    answer5 = input( "Which city is know as the pink city of India?")
+print ( "you are right")
+print ("Thanks a lot for playing") 
